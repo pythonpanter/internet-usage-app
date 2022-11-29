c0c0db2d7f0a2c2113e7e93acd36879a1798d9f0
Kai Braeunig <kaib21@pm.me>
2022-11-29 18:23:59 +0100

    tiny updates

--- internet.py
+++ internet.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from urllib.request import urlopen
